24b77086ab6391e7661f95be13f66b499cb539c8
Cyd La Luz <claluz@thinkful.com>
2020-10-06 08:17:04 -0700

    Fix repeat headers, format fix

--- src/App.js
+++ src/App.js
@@ -1,92 +1,102 @@
-import React, { Component } from 'react';
-import Customize from './Customize.js'
-import Costs from './Costs.js';
-import Total from './Total.js';
-import './App.css';
+import React, { Component } from "react";
+import Customize from "./Customize.js";
+import Costs from "./Costs.js";
+import Total from "./Total.js";
+import "./App.css";
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
-import slugify from 'slugify';
+import slugify from "slugify";
 
-import './App.css';
+import "./App.css";
 
 // This object will allow us to
 // easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
 });
 
 class App extends Component {
   state = {
     selected: {
       Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
+        name: "17th Generation Intel Core HB (7 Core with donut spare)",
+        cost: 700,
       },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
+      "Operating System": {
+        name: "Ubuntu Linux 16.04",
+        cost: 200,
       },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
+      "Video Card": {
+        name: "Toyota Corolla 1.5v",
+        cost: 1150.98,
       },
       Display: {
         name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
-      }
-    }
+        cost: 1500,
+      },
+    },
   };
 
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
     this.setState({
-      selected
+      selected,
     });
   };
 
- render() {
-
+  render() {
     const total = Object.keys(this.state.selected).reduce(
       (acc, curr) => acc + this.state.selected[curr].cost,
       0
     );
-
-    return Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature]
 
     return (
       <div className="App">
         <header>
           <h1>ELF Computing | Laptops</h1>
         </header>
-        <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-           <Customize 
-              features={this.props.features}
-              feature={selectedOption}
-              updateFeature={this.updateFeature}
-              selected={feature}
-              currencyFormat={USCurrencyFormat}/>
-          </form>
-          <section className="main__summary">
-            <h2>Your cart</h2>
-           <Costs featureHash={featureHash}
-                  selectedOption={selectedOption}
-                  feature={feature}
-                  currencyFormat={USCurrencyFormat} />
-           <Total total={total}
-                  currencyFormat={USCurrencyFormat}/>
-          </section>
-        </main>
+
+        {Object.keys(this.state.selected).map((feature, idx) => {
+          const featureHash = feature + "-" + idx;
+          const selectedOption = this.state.selected[feature];
+          return (
+            <main>
+              <form className="main__form">
+                <h2>Customize your laptop</h2>
+                <Customize
+                  features={this.props.features}
+                  feature={selectedOption}
+                  updateFeature={this.updateFeature}
+                  selected={feature}
+                  currencyFormat={USCurrencyFormat}
+                />
+              </form>
+            </main>
+          );
+        })}
+        <section className="main__summary">
+          <h2>Your cart</h2>
+          {Object.keys(this.state.selected).map((feature, idx) => {
+            const featureHash = feature + "-" + idx;
+            const selectedOption = this.state.selected[feature];
+            return (
+              <Costs
+                featureHash={featureHash}
+                selectedOption={selectedOption}
+                feature={feature}
+                currencyFormat={USCurrencyFormat}
+              />
+            );
+          })}
+        </section>
+
+        <Total total={total} currencyFormat={USCurrencyFormat} />
       </div>
-    )});
+    );
   }
 }
-
 
 export default App;
